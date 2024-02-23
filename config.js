f8b08d46a99e57c6bc795dec9f6dce1ceacd5e72
Ashif <144139175+Ashif-107@users.noreply.github.com>
2024-02-23 21:02:53 +0530

    login signup completed

---
+++ config.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const connect = mongoose.connect("mongodb://localhost:27017/chatapp");
+
+//checking the DB is connected or not
+
+connect.then(()=>{
+    console.log("DataBase Connected");
+})
+
+.catch(()=>{
+    console.log("DataBase not Connected");
+})
+
+
+//creating a schema :)
+
+const LoginSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    password:{
+        type:String,
+        required: true
+    }
+
+});
+
+
+//collecting part
+
+const Collection = new mongoose.model("users",LoginSchema);
+
+module.exports = Collection;
--- server.js
+++ server.js
@@ -17,3 +17,79 @@ io.on('connection', socket => {
     })
 });
 
+
+const express = require('express')
+const bcrypt = require('bcrypt');
+const app = express()
+const Collection = require('./config')
+app.use(express.json());
+
+app.set('views','./views')
+app.set('view engine','ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}));
+
+app.get('/',(req,res) => {
+    res.render('login')
+})
+app.get("/signup",(req,res) =>{
+    res.render("signup");
+})
+
+
+
+
+
+//register user
+
+app.post("/signup", async (req,res)=>{
+    const data = {
+        name: req.body.username,
+        password: req.body.password,
+    }
+
+
+    //check existing user
+
+    const existingUser = await Collection.findOne({name:data.name});
+
+    if(existingUser){
+        res.send("User Already Exists");
+    }
+    else{
+
+        //hash the password
+
+        const saltrounds = 10;
+        const hashpass = await bcrypt.hash(data.password,saltrounds);
+        data.password = hashpass;
+
+
+        const userdata = await Collection.insertMany(data);
+        console.log(userdata);
+    }
+
+    
+})
+
+app.post("/login", async(req,res)=>{
+    try{
+        const check = await Collection.findOne({name: req.body.username});
+        if(!check){
+            res.send("Username Cannot find");
+        }
+        const ispass = await bcrypt.compare(req.body.password, check.password);
+        if(ispass){
+            res.render("index"); 
+        }else{
+            res.send("Wrong pasword");
+        }
+    }
+    catch{
+        res.send("Wrong Details");
+    }
+})
+
+
+
+app.listen(5000)
